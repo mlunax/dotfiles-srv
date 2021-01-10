5931b60dd47f3796aef6011d3fefc5d11e527970
Luna Tsuki <lunax@mlunax.com>
2021-01-10 01:29:24 +0100

    repair install sh

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -3,7 +3,6 @@ alias \
     zshrc='source ~/.zshrc' \
     dps='docker ps -a --format "table {{.Names}}\t{{.Image}}\t{{.Status}}"' \
     c='docker-compose' \
-    dots='git --git-dir=$HOME/.dotfiles --work-tree=$HOME' \
     lla='ls -lha' \
     cp='cp -iv' \
     rm='rm -iv' \
--- install.zsh
+++ install.zsh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
+# env, because some OSes keep zsh in /bin (I'm looking at you, Alpine)
 cp .zshrc $HOME
 if [ ! -d "${HOME}/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -6,7 +7,8 @@ fi
 if [ ! -d "${HOME}/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 fi
-if [ ! -d "${HOME}/.dotfiles" ]; then
-  git clone --bare https://github.com/mlunax/dotfiles-srv $HOME/.dotfiles
+if [ -d "${HOME}/.zsh" ]; then
+  rm -rf $HOME/.zsh
 fi
+cp -r .zsh/ $HOME
 source $HOME/.zshrc
